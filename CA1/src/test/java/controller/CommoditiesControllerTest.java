2c8dc674c4aa83f8ed1dacabe4c37f94ea501792
Emad Emami <emamiemad8@gmail.com>
2023-11-28 23:40:24 +0330

    CA4

--- CA1/src/test/java/controller/CommoditiesControllerTest.java
+++ CA1/src/test/java/controller/CommoditiesControllerTest.java
@@ -21,7 +21,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class CommoditiesControllerTest {
-
     @InjectMocks
     private CommoditiesController commoditiesController;
 
